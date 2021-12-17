27a7e982c1f0c11741a78832ee4de792e9897155
haodongfang-source <dongfang7212@gmail.com>
2021-12-17 15:08:20 +0800

    finished update test

--- src/test/java/com/atguigu/mybatis_plus/MybatisPlusApplicationTests.java
+++ src/test/java/com/atguigu/mybatis_plus/MybatisPlusApplicationTests.java
@@ -14,6 +14,18 @@ class MybatisPlusApplicationTests {
     @Autowired
     private UserMapper userMapper;
 
+    // 修改
+    @Test
+    public void update() {
+        User user = new User();
+        user.setId(1471526460698140674L); // insert Long convert  Integer
+        user.setName("lucy_marry");
+
+        int count = userMapper.updateById(user);
+        System.out.println(count);
+    }
+
+    // 添加
     @Test
     public void add() {
         User user = new User();
